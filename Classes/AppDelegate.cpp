41b66f1783ef03b630039bf81fa64e1215dd6d55
Seung Ho Jang <shjang1992@hotmail.com>
2015-05-13 02:22:43 +0900

    GameScene added

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include "AppDelegate.h"
-#include "HelloWorldScene.h"
+#include "GameScene.h"
 
 USING_NS_CC;
 
@@ -48,7 +48,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = HelloWorld::createScene();
+    auto scene = GameScene::createScene();
 
     // run
     director->runWithScene(scene);
---
+++ Classes/GameScene.h
@@ -0,0 +1,24 @@
+#pragma once
+
+namespace Mighty
+{
+	class Game;
+}
+
+class GameScene : public cocos2d::Layer
+{
+public:
+	// there's no 'id' in cpp, so we recommend returning the class instance pointer
+	static cocos2d::Scene* createScene();
+
+	// Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+	virtual bool init();
+
+	// a selector callback
+	void menuCloseCallback(cocos2d::Ref* pSender);
+
+	// implement the "static create()" method manually
+	CREATE_FUNC(GameScene);
+private:
+	std::shared_ptr<Mighty::Game> game;
+};
