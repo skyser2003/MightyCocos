eb664510c13b02dd537018bcc495f5b78ce236bb
Seung Ho Jang <shjang1992@hotmail.com>
2015-05-17 00:02:34 +0900

    Dummy data removed

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -40,24 +40,6 @@ bool GameScene::init()
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-	/////////////////////////////
-	// 2. add a menu item with "X" image, which is clicked to quit the program
-	//    you may modify it.
-
-	// add a "close" icon to exit the progress. it's an autorelease object
-	auto closeItem = MenuItemImage::create(
-		"CloseNormal.png",
-		"CloseSelected.png",
-		CC_CALLBACK_1(GameScene::menuCloseCallback, this));
-
-	closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width / 2,
-		origin.y + closeItem->getContentSize().height / 2));
-
-	// create menu, it's an autorelease object
-	auto menu = Menu::create(closeItem, NULL);
-	menu->setPosition(Vec2::ZERO);
-	this->addChild(menu, 1);
-
 	for (const auto& pair : game->GetPlayers())
 	{
 		auto player = pair.second;
@@ -84,7 +66,7 @@ bool GameScene::init()
 	// add a label shows "Hello World"
 	// create and initialize a label
 
-	auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+	auto label = Label::createWithTTF("Mighty", "fonts/Marker Felt.ttf", 24);
 
 	// position the label on the center of the screen
 	label->setPosition(Vec2(origin.x + visibleSize.width / 2,
@@ -92,15 +74,6 @@ bool GameScene::init()
 
 	// add the label as a child to this layer
 	this->addChild(label, 1);
-
-	// add "GameScene" splash screen"
-	auto sprite = Sprite::create("HelloWorld.png");
-
-	// position the sprite on the center of the screen
-	sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
-
-	// add the sprite as a child to this layer
-	this->addChild(sprite, 0);
 
 	return true;
 }
