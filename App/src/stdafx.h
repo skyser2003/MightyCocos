4e836123cf3976fdf2c80156e6ff02381dd2756c
Seung Ho Jang <shjang1992@hotmail.com>
2015-06-06 20:17:18 +0900

    Project separated Project libMighty added

--- App/src/stdafx.h
+++ App/src/stdafx.h
@@ -4,14 +4,6 @@
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 #endif
 
-// C++ Standard libraries
-#include <map>
-#include <memory>
-#include <vector>
-#include <random>
-#include <chrono>
-#include <deque>
-
 // Windows Header Files
 #include <windows.h>
 #include <tchar.h>
